141ef5c147dd4321ceae0cea9c12e3d6242150f4
Juan <Juan.E.Diaz.-ND@disney.com>
2020-06-15 21:31:39 -0500

    Setup deploy tu firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          curiscofitcoach.com building...
+          curiscofitcoach.com COMING SOON
         </p>
         <a
           className="App-link"
