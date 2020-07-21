2d984eb46e0d8696051e6995ce421038d2edb239
Juan <Juan.E.Diaz.-ND@disney.com>
2020-07-20 22:32:54 -0500

    Fix plans mixin size

--- src/components/home/plans/Plans.tsx
+++ src/components/home/plans/Plans.tsx
@@ -1,36 +1,58 @@
 import React, {Component} from 'react'
-import {Container, Row} from "react-bootstrap";
+import {Container, Row} from 'react-bootstrap';
 import './Plans.sass'
-import PlanCard from "./plan-card/PlanCard";
+import PlanCard from './plan-card/PlanCard';
 
 export default class Plans extends Component {
 
     public state = {
         plans: [
-            {title: 'Month', status: 'show', details: false, description: 'description of the plan'},
-            {title: 'Three month', status: 'show', details: false, description: 'description of the plan'},
-            {title: 'Training Online', status: 'show', details: false, description: 'description of the plan'}
+            {
+                title: 'Month',
+                status: 'show',
+                details: false,
+                shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, harum magnam. Architecto deleniti iusto laborum maiores, minus nisi, provident rerum,',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, harum magnam. Architecto deleniti iusto laborum maiores, minus nisi, provident rerum, sequi temporibus totam ullam voluptatum. Aut fuga ipsum laudantium quasi!',
+                price: 99.9
+            },
+            {
+                title: 'Three month',
+                status: 'show',
+                details: false,
+                shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, harum magnam. Architecto deleniti iusto laborum maiores, minus nisi, provident rerum,',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, harum magnam. Architecto deleniti iusto laborum maiores, minus nisi, provident rerum, sequi temporibus totam ullam voluptatum. Aut fuga ipsum laudantium quasi!',
+                price: 0.0
+            },
+            {
+                title: 'Training Online',
+                status: 'show',
+                details: false,
+                shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, harum magnam. Architecto deleniti iusto laborum maiores, minus nisi, provident rerum,',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, harum magnam. Architecto deleniti iusto laborum maiores, minus nisi, provident rerum, sequi temporibus totam ullam voluptatum. Aut fuga ipsum laudantium quasi!',
+                price: 0.0
+            }
         ],
         showDetails: false
     }
 
     showPlanDetails(name: string): void {
         this.state.plans.forEach(plan => {
-            plan.details = plan.details? false : name === plan.title;
+            plan.details = plan.details ? false : name === plan.title;
         })
-        this.setState(this.state)
-        console.log(this.state);
+        this.state.showDetails = this.state.plans.some(plan => plan.details);
+        this.setState(this.state);
     }
 
     render() {
         return (
             <Container id='plans' fluid className="plans-panel">
+
                 <Row>
                     {this.state.plans
                         .filter(plan => plan.status === 'show')
                         .map((plan: any) => {
-                            return <PlanCard title={plan.title} key={plan.title} description={plan.description}  showDetails={plan.details}
-                                             onClick={this.showPlanDetails.bind(this, plan.title)} />
+                            return <PlanCard plan={plan} showDetails={this.state.showDetails} key={plan.title}
+                                             onClick={this.showPlanDetails.bind(this, plan.title)}/>
                         })}
                 </Row>
             </Container>
--- src/components/home/plans/plan-card/PlanCard.tsx
+++ src/components/home/plans/plan-card/PlanCard.tsx
@@ -1,49 +1,51 @@
 import React, {Component} from 'react'
-import Col from "react-bootstrap/Col";
-import {Button} from "react-bootstrap";
+import Col from 'react-bootstrap/Col';
+import {Button} from 'react-bootstrap';
 
 interface PlanProps {
-    title: string,
-    description: string,
+    plan: any,
     showDetails: boolean
     onClick: () => void
 }
+
 const detailStyle = {
     padding: '.5em'
 }
 
 export default class PlanCard extends Component<PlanProps> {
 
-    buildDetail() {
+    buildDetail(plan: any) {
         return (
             <div style={detailStyle}>
-                <p>Detail of plan </p>
+                <p>{plan.description}</p>
+                <p>COP {plan.price}</p>
             </div>
         );
     }
 
     render() {
-        const {title, description, onClick, showDetails} = this.props;
+        const {onClick, plan, showDetails} = this.props;
+        const {details, title, shortDescription} = plan;
 
         let size = (details: boolean) => {
             if (details) {
-                return {'xs': 9}
+                return {'md': 9}
             } else {
-                return {}
+                return  {'md': true}
             }
         };
 
         return (
-            <Col className={`plan-card`} key={title} {...size(showDetails)}>
-                <div className='d-flex'>
+            <Col className={`plan-card`} key={title} {...size(details)}>
+                <div className='d-flex flex-row' style={{width: '100%'}}>
                     <div className='d-flex flex-column'>
                         <h3>{title}</h3>
-                        <p>{description}</p>
-                        <Button onClick={onClick} className='align-self-end' variant='link'>
-                            {showDetails ? 'Hide' : 'Details'}
-                        </Button>
+                         <p>{!showDetails && shortDescription}</p>
                     </div>
-                    {showDetails && this.buildDetail()}
+                    {details && this.buildDetail(plan)}
+                    <Button onClick={onClick} className='align-self-end' variant='link'>
+                        {details ? 'Hide' : 'Details'}
+                    </Button>
                 </div>
             </Col>
         );
